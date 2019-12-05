365645b886f43e59830f1d408306c274f20b0e8c
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2019-12-05 10:08:27 +0000

    Passes Second test and third test

--- src/main/java/streams/numbersList.java
+++ src/main/java/streams/numbersList.java
@@ -1,24 +1,18 @@
 package streams;
 
+import java.util.Collections;
 import java.util.List;
 
 public class numbersList {
 
 	public static int getMax(List<Integer> numbers) {
-		int max =0;
-		for (int i =0; i< numbers.size();i++) {
-			
-			max = Math.max(max, i);
-		}
+		int max = Collections.max(numbers);
 		return max;
+		
 	}
 
 	public static int getMin(List<Integer> numbers) {
-		int min = 11;
-		for (int i =0; i< numbers.size();i++) {
-			
-			min = Math.min(min, i);
-		}
+		int min = Collections.min(numbers);
 		return min;
 	}
 
